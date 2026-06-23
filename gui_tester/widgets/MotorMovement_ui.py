15749933b17fdebb82bd9618137ec090444e135a
mlitton10 <mattlitton10@gmail.com>
2026-06-23 12:31:55 -0700

    import text box

--- gui_tester/widgets/MotorMovement_ui.py
+++ gui_tester/widgets/MotorMovement_ui.py
@@ -7,6 +7,7 @@ from PyQt5 import QtCore
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from basic_templates.TextInputBox import TextInputBox
 
 class MotorMovement(QGroupBox):
 
