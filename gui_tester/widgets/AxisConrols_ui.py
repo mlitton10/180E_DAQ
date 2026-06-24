29619520207942380a8bc9bffe51542484dce597
mlitton10 <mattlitton10@gmail.com>
2026-06-24 11:31:12 -0700

    clear old code

--- gui_tester/widgets/AxisConrols_ui.py
+++ gui_tester/widgets/AxisConrols_ui.py
@@ -17,11 +17,6 @@ class AxisControls(QGroupBox):
 
 		self.range_settings = {'min_range':-60, 'max_range':60}
 
-		self.x_high.setRange(-60, 60)
-		self.x_low.setRange(-60, 60)
-		self.y_high.setRange(-60, 60)
-		self.y_low.setRange(-60, 60)
-
 		self.x_high = UserSpinBoxRow("x-axis range:")
 		self.x_low = UserSpinBoxRow("to: ")
 
