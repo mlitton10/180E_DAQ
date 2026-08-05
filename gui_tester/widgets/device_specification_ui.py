8375a2acdfef15cab5a515975cc9977d9116109f
mlitton10 <mattlitton10@gmail.com>
2026-08-05 15:25:46 -0700

    Create a new device specifition widget for the main application window. This widget will display a dropdown list of files for device configuration data that the user can pick from. Once selected, a signal will be emitted which can be used elsewhere.

---
+++ gui_tester/widgets/device_specification_ui.py
@@ -0,0 +1,23 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget, QLabel, QComboBox, QHBoxLayout, QGroupBox, QGridLayout
+from basic_templates.TextInputBox import DropdownRow
+
+class FileDropdownRow(QWidget):
+    fileSelected = pyqtSignal(str)
+
+    def __init__(self, label_text: str, file_paths: list[str], parent=None):
+        super().__init__(parent)
+        self.file_drop_down = DropdownRow(label_text, file_paths, parent)
+        self.build_layout()
+        self.file_drop_down.optionSelected.connect(self.fileSelected)
+
+    def build_layout(self):
+        main_layout = QGridLayout(self)
+
+        self.file_drop_down.layout().setContentsMargins(0, 0, 0, 0)
+        self.file_drop_down.layout().setSpacing(0)
+
+        main_layout.addWidget(self.file_drop_down, 0, 1)
+
+    def current_file(self) -> str:
+        return self.file_drop_down.current_option()
