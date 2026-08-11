13cb937eda84b3985b7ba086428362387f26a6b2
mlitton10 <mattlitton10@gmail.com>
2026-08-11 13:40:21 -0700

    add function to connect signals

--- gui_tester/widgets/PositionControls_ui.py
+++ gui_tester/widgets/PositionControls_ui.py
@@ -23,6 +23,7 @@ class PositionControls(QGroupBox):
 
 		self.build_layout()
 		self.initialize_boxes()
+		self.connect_signals()
 
 	def connect_signals(self):
 		self.ConfirmButton.clicked.connect(self.confirm)
