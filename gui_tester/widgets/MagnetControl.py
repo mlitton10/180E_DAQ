265489e979341b1cee10dc2094720be41c3eb325
mlitton10 <mattlitton10@gmail.com>
2026-08-11 15:30:08 -0700

    Build the tab layout

--- gui_tester/widgets/MagnetControl.py
+++ gui_tester/widgets/MagnetControl.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import (
     QWidget,
     QVBoxLayout,
-    QLabel,
+    QLabel, QGridLayout,
 )
 from gui_tester.widgets.magnet_control_widgets.CurrentDisplay import CurrentDisplay
 from gui_tester.widgets.magnet_control_widgets.CurrentRequest import CurrentRequest
@@ -13,11 +13,18 @@ class MagnetWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Magnet Controls go here"))
+        self.currentRequest = CurrentRequest()
+        self.fieldLine = FieldLine()
+        self.fieldStrength = FieldStrength()
+        self.currentDisplay = CurrentDisplay()
 
-        currentRequest = CurrentRequest()
-        fieldLine = FieldLine()
-        fieldStrength = FieldStrength()
-        currentDisplay = CurrentDisplay()
+        self.build_layout()
+
+
+    def build_layout(self):
+        layout = QGridLayout(self)
+        layout.addWidget(self.fieldLine, 0,0)
+        layout.addWidget(self.fieldStrength, 1,0)
+        layout.addWidget(self.currentDisplay, 0,1)
+        layout.addWidget(self.currentRequest, 1,1)
 
