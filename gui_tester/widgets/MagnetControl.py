9be5a3a486e19610d71ca42cad5b7f1eeccbf747
mlitton10 <mattlitton10@gmail.com>
2026-08-11 15:20:18 -0700

    add current display

--- gui_tester/widgets/MagnetControl.py
+++ gui_tester/widgets/MagnetControl.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QLabel,
 )
+from gui_tester.widgets.magnet_control_widgets.CurrentDisplay import CurrentDisplay
 from gui_tester.widgets.magnet_control_widgets.CurrentRequest import CurrentRequest
 from gui_tester.widgets.magnet_control_widgets.FieldLine import FieldLine
 from gui_tester.widgets.magnet_control_widgets.FieldStrength import FieldStrength
@@ -18,3 +19,4 @@ class MagnetWidget(QWidget):
         currentRequest = CurrentRequest()
         fieldLine = FieldLine()
         fieldStrength = FieldStrength()
+        currentDisplay = CurrentDisplay()
