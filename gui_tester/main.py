347cd2e3ac49eb461916831f4391074c8683285a
mlitton10 <mattlitton10@gmail.com>
2026-08-12 11:18:23 -0700

    Get manget geometry in main

--- gui_tester/main.py
+++ gui_tester/main.py
@@ -1,20 +1,21 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QTabWidget, QApplication
+from PyQt5.QtWidgets import QMainWindow, QTabWidget, QApplication, QDesktopWidget
 from gui_tester.utils.file_util import list_data_files
 from gui_tester.widgets.MagnetControl import MagnetWidget
 from gui_tester.widgets.experimentcontrol import ExperimentControl
+from gui_tester.widgets.magnet_control_widgets.workers.MagnetGeometry import MagnetGeometry
 
 
 class MainWindow(QMainWindow):
-	def __init__(self, machine_config_paths):
+	def __init__(self, machine_config_paths, magnet_geometry_dir):
 		super(MainWindow, self).__init__()
 
 		self.setWindowTitle("DAQ and Controls")
-
+		geometry = MagnetGeometry(magnet_geometry_dir)
 		tabs = QTabWidget()
 		tabs.addTab(ExperimentControl(machine_config_paths), "ExperimentControl")
-		tabs.addTab(MagnetWidget(), "Magnets")
+		tabs.addTab(MagnetWidget(geometry), "Magnets")
 
 		self.setCentralWidget(tabs)
 
@@ -22,8 +23,9 @@ def main():
 	app = QApplication(sys.argv)
 
 	machine_configuration_dir = "./data/machine_configurations/"
+	magnet_geometry_dir = "./magnet_control_widgets/data/"
 	machine_config_paths = list_data_files(machine_configuration_dir)
-	window = MainWindow(machine_config_paths)
+	window = MainWindow(machine_config_paths, magnet_geometry_dir)
 
 	window.show()
 
