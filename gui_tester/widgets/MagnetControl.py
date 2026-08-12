bc125263f2ca95f38a8b9a6e8413ac11d0746581
mlitton10 <mattlitton10@gmail.com>
2026-08-12 12:08:17 -0700

    Add the magnet geometry loader here

--- gui_tester/widgets/MagnetControl.py
+++ gui_tester/widgets/MagnetControl.py
@@ -8,7 +8,22 @@ from gui_tester.widgets.magnet_control_widgets.CurrentControlWidget import Curre
 from gui_tester.widgets.magnet_control_widgets.FieldLine import FieldLine
 from gui_tester.widgets.magnet_control_widgets.FieldStrength import FieldStrength
 
+import pickle
 
+
+class MagnetGeometry:
+    def __init__(self, magnet_data_dir):
+        self.data_dir = magnet_data_dir
+        self.section_1_geometry = self.load("section_1_geometry.pkl")
+        self.section_2_geometry = self.load("section_2_geometry.pkl")
+        self.section_3_geometry = self.load("section_3_geometry.pkl")
+
+    def load(self, filename):
+        with open(self.data_dir + filename, 'rb') as file:
+            # Load the data from the file
+            loaded_data = pickle.load(file)
+
+        return loaded_data
 
 class MagnetWidget(QWidget):
     def __init__(self, geometry):
