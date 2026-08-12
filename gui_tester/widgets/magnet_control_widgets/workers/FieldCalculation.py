2a6b5475142aca546329459bb3099bc33242d079
mlitton10 <mattlitton10@gmail.com>
2026-08-12 11:33:05 -0700

    Add precomputed field file load and apply currents

--- gui_tester/widgets/magnet_control_widgets/workers/FieldCalculation.py
+++ gui_tester/widgets/magnet_control_widgets/workers/FieldCalculation.py
@@ -1,25 +1,38 @@
+import numpy as np
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject
 
 import pickle
 
-# Open the file in read-binary mode
-with open('data.pkl', 'rb') as file:
-    # Load the data from the file
-    loaded_data = pickle.load(file)
 
-print(loaded_data)
+def calculate_magnetic_field(currents):
+    with open('../data/section_1_fields.pkl', 'rb') as file:
+        # Load the data from the file
+        section_1_fields = pickle.load(file)
+    with open('../data/section_2_fields.pkl', 'rb') as file:
+        # Load the data from the file
+        section_2_fields = pickle.load(file)
+    with open('../data/section_3_fields.pkl', 'rb') as file:
+        # Load the data from the file
+        section_3_fields = pickle.load(file)
 
-def calculate_magnetic_field(current):
+    fields = [section_1_fields, section_2_fields, section_3_fields]
+    total_field_br = np.zeros(section_1_fields['total']['Br'].shape)
+    total_field_bz = np.zeros(section_1_fields['total']['Bz'].shape)
 
+    for n,I in enumerate(currents):
+        total_field_br += I * fields[n]['total']['Br']
+        total_field_bz += I * fields[n]['total']['Bz']
+
+    return total_field_br, total_field_bz
 
 class FieldCalculationWorker(QObject):
     finished = pyqtSignal(object)
     failed = pyqtSignal(str)
 
-    @pyqtSlot(float)
+    @pyqtSlot(float, float, float)
     def calculate(self, current):
         try:
-            result = calculate_magnetic_field(current)
+            total_field_br, total_field_bz = calculate_magnetic_field(current)
             self.finished.emit(result)
         except Exception as exc:
             self.failed.emit(str(exc))
