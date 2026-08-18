aa2b2ebf2d71b1cda11f316d76021d51da286d9f
Matt <mattlitton10@gmail.com>
2026-08-18 16:09:35 -0700

    clean up

--- gui_tester/controllers/edge_code/rpi_magnet_controller.py
+++ gui_tester/controllers/edge_code/rpi_magnet_controller.py
@@ -1,14 +1,30 @@
+import argparse
+
 import RPi.GPIO as GPIO
 from time import sleep
 
+FREQ = 1000.0
+I1_MAX = 10
+I2_MAX = 10
+I3_MAX = 100
 
+def arg_parser() -> list[float]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("I1", type=float)
+    parser.add_argument("I2", type=float)
+    parser.add_argument("I3", type=float)
+
+    args = parser.parse_args()
+
+    return [args.I1, args.I2, args.I3]
 
 class PWMPin:
-    def __init__(self, pin_number, frequency):
+    def __init__(self, pin_number: int, frequency: float) -> None:
         self.controlPin = pin_number
         self.frequency = frequency
 
         self.pwm = GPIO.PWM(self.controlPin, self.frequency)
+        self._initialize_pin()
 
     def _initialize_pin(self):
         GPIO.setwarnings(False)
@@ -17,16 +33,26 @@ class PWMPin:
         self.pwm.start(0)
         pass
 
-    def set_duty_cycle(self, duty_cycle):
+    def set_duty_cycle(self, duty_cycle: float | int) -> None:
         self.pwm.ChangeDutyCycle(duty_cycle)
 
 
-class PSUController:
-    def __init__(self, pin_numbers, frequency):
-        self.psu_1 = PWMPin(pin_numbers[0], frequency)
-        self.psu_2 = PWMPin(pin_numbers[1], frequency)
-        self.psu_3 = PWMPin(pin_numbers[2], frequency)
+class PSUController(PWMPin):
+    def __init__(self, pin_number, current: float, max_current: float) -> None:
+        super().__init__(pin_number, FREQ)
+
+        self.current = current
+
+    def convert_current_to_duty_cycle(self, duty_cycle: float) -> float:
 
 
 
 
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
